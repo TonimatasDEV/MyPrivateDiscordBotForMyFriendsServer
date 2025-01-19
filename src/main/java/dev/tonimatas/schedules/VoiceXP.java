6181db2b8175d4d8ebf493cf12d1254737a55903
TonimatasDEV <tonimatasdev@proton.me>
2025-01-19 15:26:57 +0100

    Do some stuff

---
+++ src/main/java/dev/tonimatas/schedules/VoiceXP.java
@@ -0,0 +1,38 @@
+package dev.tonimatas.schedules;
+
+import dev.tonimatas.Main;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
+
+import java.util.concurrent.TimeUnit;
+
+public class VoiceXP extends Thread {
+    public static boolean STOP = false;
+    
+    @Override
+    public void run() {
+        while (!STOP) {
+            try {
+                TimeUnit.SECONDS.sleep(30);
+            } catch (InterruptedException e) {
+                System.out.println();
+            }
+
+            Guild guild = Main.JDA.getGuildById("1166787850235289693");
+            
+            if (guild == null) continue;
+            
+            for (GuildVoiceState guildVoiceState : guild.getVoiceStates()) {
+                if (guildVoiceState == null) continue;
+                if (guildVoiceState.isDeafened()) continue;
+                if (!guildVoiceState.inAudioChannel()) continue;
+
+                if (guildVoiceState.isMuted()) {
+                    // TODO: Give muted XP 1/3
+                } else {
+                    // TODO: Give XP
+                }
+            }
+        }
+    }
+}
