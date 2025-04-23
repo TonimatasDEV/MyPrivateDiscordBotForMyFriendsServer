fa27824fbce4252123a2fe65f4ca0abd95e89ea9
DaniPraivet <danipraivet@gmail.com>
2025-04-23 12:21:30 +0200

    Add the amount of money lost as suggested

--- src/main/java/dev/tonimatas/roulette/Bet.java
+++ src/main/java/dev/tonimatas/roulette/Bet.java
@@ -21,7 +21,7 @@ public record Bet(String id, long money, BetType type, int value) {
             if (multiplier > 0) {
                 text = member.getEffectiveName() + " has ganado " + profit + "€ con tu apuesta al " + value + " (" + type + ").";
             } else {
-                text = member.getEffectiveName() + " tu apuesta al " + value + " (" + type + ") no ha ganado esta vez.";
+                text = member.getEffectiveName() + " tu apuesta al " + value + " (" + type + ") no ha ganado esta vez.\nHas perdido " + money + ".";
             }
             Getters.getRouletteChannel().sendMessage(text).queue();
         }
