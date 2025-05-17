88127ab592fa090c5b1b6b8915d744d9a81b1196
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2025-05-17 18:25:40 +0200

    chore: Add TODO

--- src/main/java/dev/tonimatas/listeners/RouletteListener.java
+++ src/main/java/dev/tonimatas/listeners/RouletteListener.java
@@ -2,19 +2,10 @@ package dev.tonimatas.listeners;
 
 import dev.tonimatas.tasks.RouletteTask;
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.interactions.components.ActionRow;
-import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu;
-import net.dv8tion.jda.api.interactions.components.text.TextInput;
-import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
-import net.dv8tion.jda.api.interactions.modals.Modal;
-import net.dv8tion.jda.api.interactions.modals.ModalMapping;
 
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 
 public class RouletteListener extends ListenerAdapter {
@@ -30,7 +21,7 @@ public class RouletteListener extends ListenerAdapter {
 
         switch (event.getFullCommandName()) {
             case "bet" -> {
-                event.reply("Choose your bet").setEphemeral(true).addActionRow(getBetTypeSelection()).queue();
+                // TODO
             }
 
             case "money" -> {
@@ -70,69 +61,5 @@ public class RouletteListener extends ListenerAdapter {
                 }
             }
         }
-    }
-
-    @Override
-    public void onStringSelectInteraction(StringSelectInteractionEvent event) {
-        if (event.getComponentId().equals("roulette-bet-type-menu")) {
-            String type = event.getId();
-            event.replyModal(getOptions(type)).queue();
-            event.getMessage().delete().queue();
-        }
-    }
-
-    @Override
-    public void onModalInteraction(ModalInteractionEvent event) {
-        if (event.getModalId().equals("roulette-bet-modal")) {
-            for (ModalMapping modalMapping : event.getValues()) {
-                event.reply(modalMapping.getAsString()).setEphemeral(true).queue();
-            }
-        }
-    }
-
-    private Modal getOptions(String type) {
-        TextInput option = TextInput.create("roulette-bet-" + type, "What " + type + " do you want to bet?", TextInputStyle.SHORT)
-                .setPlaceholder("1,2,3,4,5...")
-                .setMinLength(1)
-                .setMaxLength(2)
-                .build();
-        
-        return Modal.create("roulette-bet-modal", "Bet").addActionRow(option).build();
-    }
-    
-    private StringSelectMenu getBetTypeSelection() {
-        return StringSelectMenu.create("roulette-bet-type-menu")
-                .addOption("Color", "color")
-                .addOption("Column", "column")
-                .addOption("Dozen", "dozen")
-                .addOption("Number", "number")
-                .build();
-    }
-
-    private StringSelectMenu getOptionSelection(String type) {
-        StringSelectMenu.Builder builder = StringSelectMenu.create("roulette-bet-option-menu");
-        
-        switch (type) {
-            case "color" -> {
-                
-            }
-            
-            case "column" -> {
-                
-            }
-            
-            case "dozen" -> {
-                
-            }
-            
-            case "number" -> {
-                for (int i = 0; i < 37; i++) {
-                    String value = String.valueOf(i);
-                    builder.addOption(value, value);
-                }
-            }
-        }
-        
-        return builder.build();
     }
 }
