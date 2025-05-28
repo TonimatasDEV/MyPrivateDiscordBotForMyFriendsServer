b1083b0505f4c748aca28f0e95e64141a3ecddd7
DaniPraivet <danipraivet@gmail.com>
2025-05-28 14:09:55 +0200

    chore: VoiceXPManager multi-loop simplified to one loop

--- src/main/java/dev/tonimatas/voicexp/VoiceXPManager.java
+++ src/main/java/dev/tonimatas/voicexp/VoiceXPManager.java
@@ -1,8 +1,8 @@
 package dev.tonimatas.voicexp;
 
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
 
 import java.util.HashMap;
 import java.util.List;
@@ -10,49 +10,55 @@ import java.util.Map;
 
 public class VoiceXPManager {
     // Map for storaging all users with their respective points
-    private final Map<String, Integer> userPoints = new HashMap<>();
+    private final Map<String, Long> userPoints = new HashMap<>();
 
     public void processVoiceActivity(Guild guild) {
-        for (VoiceChannel voiceChannel : guild.getVoiceChannels()) {
-            List<Member> members = voiceChannel.getMembers();
+        if (guild == null) return;
+
+        for (GuildVoiceState voiceState : guild.getVoiceStates()) {
+            if (voiceState.getChannel() == null || guild.getAfkChannel() == voiceState.getChannel().asVoiceChannel()) {
+                continue;
+            }
+
+            List<Member> members = voiceState.getChannel().getMembers();
             if (members.size() <= 1) continue;
 
-            for (Member member : members) {
-                if (member.getUser().isBot()) continue;
+            int defPoints = 5;
+            double multiplier = getMultiplier(voiceState);
 
-                // Default number of points which it will be given to each user
-                int defPoints = 5;
-                double multiplier = getMultiplier(member);
+            // Total points which it is the defPoints * Multiplier
+            long totPoints = (long) (defPoints * multiplier);
 
-                // Total points which it is the defPoints * Multiplier
-                int totPoints = (int) (defPoints * multiplier);
-
-                addPoints(member.getId(), totPoints);
-            }
+            addPoints(voiceState.getMember().getId(), totPoints);
         }
     }
 
-    private double getMultiplier(Member member) {
+    private double getMultiplier(GuildVoiceState voiceState) {
         double multiplier = 1.0;
 
-        if (member.getVoiceState().isDeafened()) multiplier *= 0.25;
-        else if (member.getVoiceState().isMuted()) multiplier *= 0.5;
-        if (member.getVoiceState().isStream()) multiplier *= 2.0;
+        if (voiceState.isDeafened()) {
+            multiplier *= 0.25;
+        } else if (voiceState.isMuted()) {
+            multiplier *= 0.5;
+        }
+        if (voiceState.isStream()) {
+            multiplier *= 2.0;
+        }
 
         return multiplier;
     }
 
-    private void addPoints(String userId, int points) {
-        userPoints.put(userId, userPoints.getOrDefault(userId, 0) + points);
+    private void addPoints(String userId, long points) {
+        userPoints.put(userId, userPoints.getOrDefault(userId, 0L) + points);
     }
 
     // Method for getting each user points (leaderboard)
-    public int getPoints(String userId) {
-        return userPoints.getOrDefault(userId,0);
+    public long getPoints(String userId) {
+        return userPoints.getOrDefault(userId, 0L);
     }
 
     // Method to get all user and their respective points (leaderboard)
-    public Map<String,Integer> getAllPoints() {
+    public Map<String, Long> getAllPoints() {
         return userPoints;
     }
 }
