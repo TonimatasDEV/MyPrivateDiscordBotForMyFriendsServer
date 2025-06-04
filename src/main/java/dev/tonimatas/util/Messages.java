b6d71099e94cfc9e5980cc7f711cf63ce1ef9b03
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2025-06-04 20:46:02 +0200

    feat: Default embed

--- src/main/java/dev/tonimatas/util/Messages.java
+++ src/main/java/dev/tonimatas/util/Messages.java
@@ -1,7 +1,12 @@
 package dev.tonimatas.util;
 
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.SelfUser;
 import net.dv8tion.jda.api.interactions.InteractionHook;
 
+import java.awt.*;
+import java.time.ZonedDateTime;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
@@ -16,5 +21,16 @@ public class Messages {
 
             hook.deleteOriginal().queue();
         };
+    }
+    
+    public static EmbedBuilder getDefaultEmbed(JDA jda) {
+        SelfUser selfUser = jda.getSelfUser();
+        
+        return new EmbedBuilder()
+                .setAuthor(selfUser.getEffectiveName(), null, selfUser.getEffectiveAvatarUrl())
+                .setColor(Color.YELLOW)
+                .setTimestamp(ZonedDateTime.now())
+                .setFooter("Anticonstitucionalmente");
+
     }
 }
