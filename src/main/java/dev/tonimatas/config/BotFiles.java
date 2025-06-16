e1cb9946149b7bda4f04a39e7dee650c496e72be
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2025-06-16 21:53:17 +0200

    Ups

--- src/main/java/dev/tonimatas/config/BotFiles.java
+++ src/main/java/dev/tonimatas/config/BotFiles.java
@@ -4,6 +4,5 @@ public class BotFiles {
     public static BotConfig CONFIG = JsonFile.loadOrCreate(BotConfig.class, "bot.json");
     public static BankData BANK = JsonFile.loadOrCreate(BankData.class, "data/bank.json");
     public static ExtraData EXTRA = JsonFile.loadOrCreate(ExtraData.class, "data/extra.json");
-    public static PaymentsData PAYMENTS = JsonFile.loadOrCreate(PaymentsData.class, "data/payments.json");
     public static PointsData POINTS = JsonFile.loadOrCreate(PointsData.class, "data/points.json");
 }
