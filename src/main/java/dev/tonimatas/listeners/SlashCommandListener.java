37af4df6a2126439a96a2a6e4f66083930e54a18
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2025-06-17 20:32:13 +0200

    chore: Extract auto complete listener

--- src/main/java/dev/tonimatas/listeners/SlashCommandListener.java
+++ src/main/java/dev/tonimatas/listeners/SlashCommandListener.java
@@ -6,11 +6,9 @@ import dev.tonimatas.systems.bank.UserSettings;
 import dev.tonimatas.systems.roulette.Roulette;
 import dev.tonimatas.systems.roulette.bets.Bet;
 import dev.tonimatas.util.Messages;
-import dev.tonimatas.util.Strings;
 import dev.tonimatas.util.TimeUtils;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.MessageEmbed;
-import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
@@ -18,7 +16,6 @@ import net.dv8tion.jda.api.interactions.components.buttons.Button;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.Collections;
 
 public class SlashCommandListener extends ListenerAdapter {
     private static final String COMMANDS_CHANNEL = "1380277341405581443";
@@ -44,34 +41,6 @@ public class SlashCommandListener extends ListenerAdapter {
             case "hi" -> executeHi(event);
             case "transactions" -> executeTransactions(event);
             case "options" -> executeOptions(event);
-        }
-    }
-
-    @Override
-    public void onCommandAutoCompleteInteraction(CommandAutoCompleteInteractionEvent event) {
-        if (event.getName().equalsIgnoreCase("bet")) {
-            String option = event.getFocusedOption().getName();
-            String focusedValue = event.getFocusedOption().getValue();
-
-            if (option.equalsIgnoreCase("bet-type")) {
-                String[] options = new String[]{"color", "column", "dozen", "number"};
-                event.replyChoices(Strings.getStartWithValues(options, focusedValue)).queue();
-            } else if (option.equalsIgnoreCase("bet-option")) {
-                OptionMapping betType = event.getOption("bet-type");
-
-                if (betType == null) return;
-                String[] options;
-
-                switch (betType.getAsString()) {
-                    case "color" -> options = new String[]{"green", "red", "black"};
-                    case "column", "dozen" -> options = new String[]{"first", "second", "third"};
-                    default -> options = new String[]{};
-                }
-
-                event.replyChoices(Strings.getStartWithValues(options, focusedValue)).queue();
-            } else {
-                event.replyChoices(Collections.emptyList()).queue();
-            }
         }
     }
 
