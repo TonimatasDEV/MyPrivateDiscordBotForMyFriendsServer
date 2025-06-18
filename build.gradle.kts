330ae1ab48b766801045b5bb6543d8e3c2f3cdc9
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2025-06-18 16:13:54 +0200

    Add application plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+import kotlin.io.path.createDirectory
+import kotlin.io.path.exists
+
 plugins {
     java
+    application
     id("com.gradleup.shadow") version "9.0.0-beta16"
 }
 
@@ -22,6 +26,16 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:1.5.18")
     // https://github.com/google/gson/releases
     implementation("com.google.code.gson:gson:2.13.1")
+}
+
+application {
+    mainClass = "dev.tonimatas.Main"
+}
+
+tasks.named<JavaExec>("run") {
+    val path = rootDir.toPath().resolve("run")
+    workingDir = path.toFile()
+    if (!path.exists()) path.createDirectory()
 }
 
 tasks.jar {
