d8f54d5cdcc0cbd3538907c0b7ddc863d13df40b
DaniPraivet <danipraivet@gmail.com>
2025-06-22 18:02:07 +0200

    feat(tictactoe): Manager class created

--- src/main/java/dev/tonimatas/systems/tictactoe/TicTacToeManager.java
+++ src/main/java/dev/tonimatas/systems/tictactoe/TicTacToeManager.java
@@ -6,7 +6,28 @@ import java.util.Map;
 public class TicTacToeManager {
     private final Map<String, TicTacToeGame> games = new HashMap<>();
 
-    public boolean startGame(String userId, Player playerX, Player playerO) {
+    public boolean isPlaying(String userId) {
+        return games.containsKey(userId);
+    }
 
+    public TicTacToeGame getGame(String userId) {
+        return games.get(userId);
+    }
+
+    public void endGame(String userId) {
+        games.remove(userId);
+    }
+
+    public boolean startGame(String userId, Player playerX, Player playerO, boolean vsBot) {
+    if (isPlaying(userId)) return false;
+    if (!vsBot && isPlaying(playerO.getMember().getId())) return false;
+
+    TicTacToeGame game = new TicTacToeGame(playerX, playerO, vsBot);
+    games.put(userId, game);
+
+    if (!vsBot) {
+        games.put(playerO.getMember().getId(), game);
+    }
+    return true;
     }
 }
