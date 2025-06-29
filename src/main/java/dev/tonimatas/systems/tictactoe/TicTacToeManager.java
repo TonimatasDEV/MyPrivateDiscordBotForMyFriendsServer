1685e3a876be7144edb8b907e70cec76807d3435
DaniPraivet <danipraivet@gmail.com>
2025-06-30 01:04:13 +0200

    chore(tictactoe): Manager class with javadocs

--- src/main/java/dev/tonimatas/systems/tictactoe/TicTacToeManager.java
+++ src/main/java/dev/tonimatas/systems/tictactoe/TicTacToeManager.java
@@ -4,8 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TicTacToeManager {
+    /**
+     * Game storage
+     */
     private final Map<String, TicTacToeGame> games = new HashMap<>();
 
+    /**
+     *
+     * @param userId user main identification
+     * @return If the match is still live, or it ended quite time ago
+     */
     public boolean isPlaying(String userId) {
         return games.containsKey(userId);
     }
@@ -14,10 +22,22 @@ public class TicTacToeManager {
         return games.get(userId);
     }
 
+    /**
+     * Remove a game from the storage
+     * @param userId user main identification
+     */
     public void endGame(String userId) {
         games.remove(userId);
     }
 
+    /**
+     * Creates a new game and starts it
+     * @param userId user main identification
+     * @param playerX player 1
+     * @param playerO player 2
+     * @param vsBot boolean
+     * @return if the parameters are correct or if something went wrong
+     */
     public boolean startGame(String userId, Player playerX, Player playerO, boolean vsBot) {
     if (isPlaying(userId)) return false;
     if (!vsBot && isPlaying(playerO.getMember().getId())) return false;
