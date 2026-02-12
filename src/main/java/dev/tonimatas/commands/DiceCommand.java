62255ea006ef318d2e4a2b0140d8d93bdb97b9c1
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2026-02-12 11:58:59 +0100

    chore: Extract RANDOM from some classes to Utils (#186)

--- src/main/java/dev/tonimatas/commands/DiceCommand.java
+++ src/main/java/dev/tonimatas/commands/DiceCommand.java
@@ -1,21 +1,19 @@
 package dev.tonimatas.commands;
 
 import dev.tonimatas.util.Messages;
+import dev.tonimatas.util.Utils;
 import net.dv8tion.jda.api.utils.messages.MessageCreateData;
 import revxrsal.commands.annotation.Command;
 import revxrsal.commands.annotation.Description;
 import revxrsal.commands.annotation.Named;
 import revxrsal.commands.jda.actor.SlashCommandActor;
 
-import java.security.SecureRandom;
-
 public class DiceCommand {
-    private static final SecureRandom RANDOM = new SecureRandom();
 
     @Command("dice")
     @Description("Get a random number!")
     public void execute(SlashCommandActor actor, @Named("min") @Description("Minimum number") Integer min, @Named("max") @Description("Maximum number") Integer max) {
-        int result = RANDOM.nextInt(min, max + 1);
+        int result = Utils.RANDOM.nextInt(min, max + 1);
         MessageCreateData embed = Messages.getDefaultEmbed_Lamp(actor.jda(), "Dice", "The number is " + result + ".");
         actor.replyToInteraction(embed).queue();
     }
